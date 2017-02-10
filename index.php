f63ab96dfe062765460640715ffe4e73fb1c567c
elvalera <elvis.tecno@gmail.com>
2017-02-10 05:30:51 -0400

    Inicio del Proyecto Iniciando la pagina sin con el comando: php yii/framework/yiic webapp web

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta http-equiv="Refresh" content="3;url=web/index.php">
+</head>
+
+<body>
+<p>Redirigiendo pagina: <br>
+Si no se redirige automaticamente has clik <a href="web/index.php">aqui</a></p>
+</body>
+</html>
