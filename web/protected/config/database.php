90564742ce2b34a0196188b55eba85ab89cc5853
elvalera <elvis.tecno@gmail.com>
2017-02-11 17:52:39 -0400

    Habilitar módulo de generación Gii pagina 19

--- web/protected/config/database.php
+++ web/protected/config/database.php
@@ -2,13 +2,13 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',//para utilizar la base de datos que viene por defecto..
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	
+	'connectionString' => 'mysql:host=217.0.0.1;dbname=yiitwitter',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'postgres',
+	'password' => 'postgres',
 	'charset' => 'utf8',
-	*/
+	
 );
