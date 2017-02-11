cdf2696ba7e1f1824b93f71bf95ed1faba839203
elvalera <elvis.tecno@gmail.com>
2017-02-11 18:19:34 -0400

    Generacion de codigo con gii modulo de usuarios hasta pag.-24

--- web/protected/config/database.php
+++ web/protected/config/database.php
@@ -2,13 +2,11 @@
 
 // This is the database connection configuration.
 return array(
-	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',//para utilizar la base de datos que viene por defecto..
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	
-	'connectionString' => 'mysql:host=217.0.0.1;dbname=yiitwitter',
+	'connectionString' => 'pgsql:host=127.0.0.1;dbname=yiitwitter',
 	'emulatePrepare' => true,
 	'username' => 'postgres',
 	'password' => 'postgres',
 	'charset' => 'utf8',
-	
 );
